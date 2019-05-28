26f007a927d8a37151e1dce9fabf9bd48ac3813c
Nikhil Srivastava <niksrc@gmail.com>
2019-05-28 10:53:32 +0530

    feat: add support for multiple commands in xo

--- xo/entrypoint.sh
+++ xo/entrypoint.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
 set -e
 
-sh -c "xo $*"
+for cmd in "$@"; do
+    echo "Running xo '$cmd'..."
+    if sh -c "xo $cmd"; then
+        # no op
+        echo "Successfully ran xo '$cmd'"
+    else
+        exit_code=$?
+        echo "Failure running xo '$cmd', exited with $exit_code"
+        exit $exit_code
+    fi
+done
