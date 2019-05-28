74f7808cdb4da038a91a5e395f2e532e764aa3f6
Nikhil Srivastava <niksrc@gmail.com>
2019-05-28 10:55:03 +0530

    feat: add multiple commands support in lerna

--- lerna/entrypoint.sh
+++ lerna/entrypoint.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
 set -e
 
-sh -c "lerna $*"
+for cmd in "$@"; do
+    echo "Running lerna '$cmd'..."
+    if sh -c "lerna $cmd"; then
+        # no op
+        echo "Successfully ran lerna '$cmd'"
+    else
+        exit_code=$?
+        echo "Failure running lerna '$cmd', exited with $exit_code"
+        exit $exit_code
+    fi
+done
