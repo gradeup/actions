913e794eb0b4b692e3647d7f1b9a9705f3c16d41
Nikhil Srivastava <niksrc@gmail.com>
2019-06-25 18:02:52 +0530

    fix(lint-js): use npx to install xo

--- lint-js/entrypoint.sh
+++ lint-js/entrypoint.sh
@@ -7,4 +7,4 @@ repo=$(echo "$GITHUB_REPOSITORY" | cut -d"/" -f2)
 pull_number=$(jq '.pull_request.number' /github/workflow/event.json)
 files=$(pr-files -owner="$owner" -repo="$repo" -pull-number="$pull_number" | grep -v public | xargs echo)
 
-sh -c "xo ${files:-package.json} --quiet"
+sh -c "npx xo ${files:-package.json} --quiet"
